19252b7f756ec58d51e3ecd6ec3ba8bdfa0db23b
smtpmailers <sandeepsingh@smtpmailers.com>
2024-10-14 16:30:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function startCaptcha() {
 
         // Redirect to the main site after a delay
         setTimeout(() => {
-            window.location.href = "https://www.flipkart.com/"; // Redirect URL
+            window.location.href = "https://www.smtpmailers.com/"; // Redirect URL
         }, 1500); // 1.5 seconds delay before redirect
     }, 2000); // Simulated processing time (2 seconds)
 }
